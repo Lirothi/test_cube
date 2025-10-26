56f75be8cbd9a698350bebd8c9f3701feb4aebe2
Lirothi <darkcheetah666@gmail.com>
2025-10-26 11:08:14 +0100

    Simplify DPI awareness module loading

--- sources/app/main.cpp
+++ sources/app/main.cpp
@@ -1,4 +1,4 @@
-﻿#include <windows.h>
+#include <windows.h>
 #include <mimalloc.h>
 #pragma warning(push)
 #pragma warning(disable: 28251)
@@ -12,6 +12,58 @@
 
 int ForceMi() { return mi_version(); }
 
+
+namespace
+{
+void EnableDpiAwareness()
+{
+#ifndef DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2
+#define DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2 ((DPI_AWARENESS_CONTEXT)-4)
+#endif
+
+    // Try to opt-in to the most precise DPI awareness available on the host OS.
+    const HMODULE user32 = GetModuleHandleW(L"user32.dll");
+    if (user32)
+    {
+        using SetProcessDpiAwarenessContextFn = BOOL(WINAPI*)(DPI_AWARENESS_CONTEXT);
+        auto setProcessDpiAwarenessContext = reinterpret_cast<SetProcessDpiAwarenessContextFn>(
+            GetProcAddress(user32, "SetProcessDpiAwarenessContext"));
+        if (setProcessDpiAwarenessContext &&
+            setProcessDpiAwarenessContext(DPI_AWARENESS_CONTEXT_PER_MONITOR_AWARE_V2))
+        {
+            return;
+        }
+    }
+
+    HMODULE shcore = GetModuleHandleW(L"shcore.dll");
+    if (!shcore)
+    {
+        shcore = LoadLibraryW(L"shcore.dll");
+    }
+    if (shcore)
+    {
+        using SetProcessDpiAwarenessFn = HRESULT(WINAPI*)(int);
+        auto setProcessDpiAwareness = reinterpret_cast<SetProcessDpiAwarenessFn>(
+            GetProcAddress(shcore, "SetProcessDpiAwareness"));
+        if (setProcessDpiAwareness &&
+            SUCCEEDED(setProcessDpiAwareness(2 /*PROCESS_PER_MONITOR_DPI_AWARE*/)))
+        {
+            return;
+        }
+    }
+
+    if (user32)
+    {
+        using SetProcessDpiAwareFn = BOOL(WINAPI*)();
+        if (auto setProcessDpiAware = reinterpret_cast<SetProcessDpiAwareFn>(
+                GetProcAddress(user32, "SetProcessDPIAware")))
+        {
+            setProcessDpiAware();
+        }
+    }
+}
+} // namespace
+
 // Entry point
 int WINAPI WinMain(
     _In_ HINSTANCE hInstance,
@@ -20,6 +72,7 @@ int WINAPI WinMain(
     _In_ int nShowCmd
 )
 {
+    EnableDpiAwareness();
     App app;
     app.Run(hInstance, nShowCmd);
     return 0;
