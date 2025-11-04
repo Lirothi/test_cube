0a72b9a7b859326bb86977811ee25a916c921150
Lirothi <darkcheetah666@gmail.com>
2025-11-04 09:45:05 +0100

    Fix cascade shadow frustum culling

--- sources/core/math/Frustum.h
+++ sources/core/math/Frustum.h
@@ -1,11 +1,13 @@
 #pragma once
 
+#include <algorithm>
+
 #include <DirectXCollision.h>
 
 #include "core/math/Math.h"
 #include "core/math/AABB.h"
 
-// Lightweight wrapper around DirectX::BoundingFrustum that exposes
+// Lightweight wrapper around DirectX collision primitives that exposes
 // intersection tests against the engine's AABB type.
 class Frustum
 {
@@ -33,6 +35,20 @@ public:
         return frustum;
     }
 
+    static Frustum FromOrthoBounds(
+        const Math::mat4& invView,
+        float minX,
+        float maxX,
+        float minY,
+        float maxY,
+        float nearPlane,
+        float farPlane)
+    {
+        Frustum frustum;
+        frustum.BuildOrtho(invView, minX, maxX, minY, maxY, nearPlane, farPlane);
+        return frustum;
+    }
+
     bool IsValid() const { return valid_; }
 
     bool Intersects(const AABB& bounds) const
@@ -50,10 +66,28 @@ public:
         DirectX::BoundingBox box{};
         box.Center = bounds.GetCenter().xf();
         box.Extents = bounds.GetHalfExtents().xf();
-        return frustum_.Intersects(box);
+
+        if (type_ == Type::Perspective)
+        {
+            return frustum_.Intersects(box);
+        }
+
+        if (type_ == Type::OrthoBox)
+        {
+            return orthoBox_.Intersects(box);
+        }
+
+        return true;
     }
 
 private:
+    enum class Type
+    {
+        Invalid,
+        Perspective,
+        OrthoBox,
+    };
+
     void Build(
         const Math::mat4& invView,
         const Math::mat4& proj,
@@ -73,10 +107,49 @@ private:
         viewFrustum.Transform(worldFrustum, invView.xm());
 
         frustum_ = worldFrustum;
+        type_ = Type::Perspective;
+        valid_ = true;
+    }
+
+    void BuildOrtho(
+        const Math::mat4& invView,
+        float minX,
+        float maxX,
+        float minY,
+        float maxY,
+        float nearPlane,
+        float farPlane)
+    {
+        const Math::float3 centerLS(
+            0.5f * (minX + maxX),
+            0.5f * (minY + maxY),
+            0.5f * (nearPlane + farPlane));
+
+        const Math::float3 halfExtents(
+            0.5f * std::max(0.0f, maxX - minX),
+            0.5f * std::max(0.0f, maxY - minY),
+            0.5f * std::max(0.0f, farPlane - nearPlane));
+
+        // Transform center into world space.
+        const Math::float3 centerWS = (invView * Math::float4(centerLS, 1.0f)).xyz();
+
+        DirectX::XMVECTOR scale{};
+        DirectX::XMVECTOR rotation{};
+        DirectX::XMVECTOR translation{};
+        DirectX::XMMatrixDecompose(&scale, &rotation, &translation, invView.xm());
+        rotation = DirectX::XMQuaternionNormalize(rotation);
+
+        orthoBox_.Center = centerWS.xf();
+        orthoBox_.Extents = halfExtents.xf();
+        DirectX::XMStoreFloat4(&orthoBox_.Orientation, rotation);
+
+        type_ = Type::OrthoBox;
         valid_ = true;
     }
 
     DirectX::BoundingFrustum frustum_{};
+    DirectX::BoundingOrientedBox orthoBox_{};
+    Type type_ = Type::Invalid;
     bool valid_ = false;
 };
 
